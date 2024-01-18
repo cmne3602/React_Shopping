deea9b29627203454a4b5273a38d1d243241c72d
cmne3602 <53000757+cmne3602@users.noreply.github.com>
2024-01-18 18:38:16 +0900

    01.18_2

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import {Button, Navbar, Container, Nav, Row, Col} from 'react-bootstrap'
 import bgroot from './bg.png';
 import { useState } from 'react';
 import data from './data.js';
-import {Routes, Route, Link} from 'react-router-dom'
-import detail from './detail.js'
+import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
+import Detail from './detail.js'
 
 function App() {
+  let [shoes, setShoes] = useState(data);
+  let navigate = useNavigate();
+
   return (
     <div className="App">
 
@@ -15,32 +18,46 @@ function App() {
         <Container>
         <Navbar.Brand href="#home">Shopping Mall</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link onClick={()=>{
+            navigate('/')
+          }}>Home</Nav.Link>
+          <Nav.Link onClick={()=>{
+            navigate('/detail/0')
+          }}>Detail</Nav.Link>
         </Nav>
         </Container>
       </Navbar>
-      <Link to="/">홈</Link>
-      <Link to="/detail">상세</Link>
+      {/* <Link to="/">홈</Link>
+      <Link to="/detail">상세</Link> */}
       <Routes>
-        <Route path='/detail' element={<div>{detail()}</div>}/>
-        <Route path='/' element={<div><Main></Main></div>}/>
+        <Route path='/' element={<div><Main shoes={shoes}></Main></div>}/>
+        <Route path='/detail/:id' element={<div>{<Detail shoes={shoes}></Detail>}</div>}/>
+
+        <Route path='/about' element={<About></About>}>
+          <Route path='member' element={<div>멤버임</div>}></Route>
+          <Route path='location' element={<div>위치정보임</div>}></Route>
+        </Route>
+        <Route path='/event' element={<Event></Event>}>
+          <Route path='one' element={<div>첫 주문 시 양배추즙 서비스</div>}></Route>
+          <Route path='two' element={<div>생일 기념 쿠폰 받기</div>}></Route>
+        </Route>
+
+        <Route path='*' element={<div>없는페이지요</div>}></Route>
       </Routes>
 
     </div>
   );
 }
 
-function Main() {
-  let [shoes, setShoes] = useState(data);
+function Main(props) {
+
   return(
     <>
       <div className='main-bg' style={{backgroundImage : 'url('+bgroot+'  )'}}></div>
       <Container>
         <Row>
           {
-            shoes.map((a, i)=>{
+            props.shoes.map((a, i)=>{
               return(
                 <Col>
                 <Item obj={data[i]} url={'https://codingapple1.github.io/shop/shoes'+(i+1)+'.jpg'}></Item>
@@ -60,6 +77,24 @@ function Item(props) {
       <img src={props.url} width='80%'/>
       <h4>{ props.obj.title }</h4>
       <p>{ props.obj.content }</p>
+    </div>
+  )
+}
+
+function About() {
+  return(
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function Event() {
+  return(
+    <div>
+      오늘의 이벤트
+      <Outlet></Outlet>
     </div>
   )
 }
--- src/detail.js
+++ src/detail.js
@@ -1,14 +1,36 @@
-let detail = function(){
+import { useParams } from "react-router-dom";
+import styled from 'styled-components'
+
+let Btn = styled.button`
+    background : ${ props=>props.bg }};
+    color : ${ props=>props.bg == 'blue' ? 'white' : 'black'};
+    padding : 10px;`
+
+let Box = styled.div`
+    background : grey;
+    padding : 20px;
+`
+let NewBtn = styled.button(Btn)
+
+
+let Detail = function(props){
+
+    let {id} = useParams();
+    console.log(id)
     return(
         <div className="container">
+            <Box>
+                <Btn bg='blue'>버튼</Btn>
+                <Btn bg='orange'>버튼</Btn>
+            </Box>
             <div className="row">
                 <div className="col-md-6">
-                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+                    <img src={"https://codingapple1.github.io/shop/shoes"+(Number(id)+1)+".jpg"} width="100%" />
                 </div>
                 <div className="col-md-6">
-                    <h4 className="pt-5">상품명</h4>
-                    <p>상품설명</p>
-                    <p>120000원</p>
+                    <h4 className="pt-5">{props.shoes[id].title}</h4>
+                    <p>{props.shoes[id].content}</p>
+                    <p>{props.shoes[id].price}</p>
                     <button className="btn btn-danger">주문하기</button> 
                 </div>
             </div>
@@ -16,4 +38,4 @@ let detail = function(){
     )
 }
 
-export default detail;
+export default Detail;
